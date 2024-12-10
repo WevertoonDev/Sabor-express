f61c7844ab8c1812b10622780132fd368e6c4595
weverton alves <wevertonm22@iclou.com>
2024-12-09 22:54:08 -0300

    Alterando função

--- app.py
+++ app.py
@@ -13,5 +13,12 @@ print('3. Ativar restaurante')
 print('4.sair\n')
 
 opcao_escolhoida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção{opcao_escolhoida}')
 
+if opcao_escolhoida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhoida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhoida == 3:
+    print('Ativar restaurantes')
+else:
+    print('Encerrar o programa')
